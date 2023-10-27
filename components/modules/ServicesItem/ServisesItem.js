91c9a8b2aecc2cf73083c76287587bd01a59e112
amir4976 <amirhosianahmadi4976@gmail.com>
2023-10-27 20:01:55 +0330

    Folder structure

---
+++ components/modules/ServicesItem/ServisesItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServisesItem() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ServisesItem
---
+++ components/templates/Services/OurServices.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurServices() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OurServices
