d2b8c19991ec63198c92dc61cf87e7cad70d0456
amir4976 <amirhosianahmadi4976@gmail.com>
2023-11-02 19:32:31 +0330

    menu

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Slider from '@/components/templates/Index/Slider'
 import About from '@/components/templates/Index/About'
 import Services from '@/components/templates/Index/Services'
 import Offer from '@/components/templates/Index/Offer'
-
+import Menu from '@/components/templates/Index/Menu'
 export default function Home() {
   return (
    <>
@@ -12,6 +12,7 @@ export default function Home() {
   <About/>
   <Services/> 
   <Offer/>
+  <Menu/>
    </>
   )
 }
