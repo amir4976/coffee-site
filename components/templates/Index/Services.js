a6ec257645ff7762b51dc621db8a041d138c70e1
amir4976 <amirhosianahmadi4976@gmail.com>
2023-11-06 20:48:02 +0330

    adding db

--- components/templates/Index/Services.js
+++ components/templates/Index/Services.js
@@ -1,8 +1,9 @@
 import ServisesItem from '@/components/modules/ServicesItem/ServisesItem'
 import React from 'react'
 
-function Services() {
-  return (
+function Services({services}) {
+  console.log(services)
+return (
 <div class="container-fluid pt-5">
         <div class="container">
             <div class="section-title">
@@ -10,12 +11,13 @@ function Services() {
                 <h1 class="display-4">Fresh & Organic Beans</h1>
             </div>
             <div class="row">
-
-                <ServisesItem img={'service-1.jpg'} title={'Best Quality Coffee'} dis={'Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit invidunt, dolore tempor diam ipsum takima erat tempor'}/>
-                <ServisesItem img={'service-2.jpg'} title={'Fresh Coffee Beans'} dis={'Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit invidunt, dolore tempor diam ipsum takima erat tempor'}/>
+                {services.map((serve)=>(
+                  <ServisesItem key={serve.id} img={serve.img} title={serve.title} dis={serve.desc}/>
+                ))}
+                {/* <ServisesItem img={'service-2.jpg'} title={'Fresh Coffee Beans'} dis={'Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit invidunt, dolore tempor diam ipsum takima erat tempor'}/>
                 <ServisesItem img={'service-3.jpg'} title={'Online Table Booking'} dis={'Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit invidunt, dolore tempor diam ipsum takima erat tempor'}/>
                 <ServisesItem img={'service-4.jpg'} title={'Best Quality Coffee'} dis={'Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit invidunt, dolore tempor diam ipsum takima erat tempor'}/>
-               
+                */}
             </div>
         </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,28 @@ import About from '@/components/templates/Index/About'
 import Services from '@/components/templates/Index/Services'
 import Offer from '@/components/templates/Index/Offer'
 import Menu from '@/components/templates/Index/Menu'
-export default function Home() {
+export default function Home(props) {
+  console.log(props.res)
   return (
    <>
   <Slider/>
   <About/>
-  <Services/> 
+  <Services services={props.resService}/> 
   <Offer/>
-  <Menu/>
+  <Menu menu={props.resMenu}/>
    </>
   )
+}
+
+export async function getStaticProps() {
+  const FetchDataService = await fetch('http://localhost:3000/services')
+  const resService = await FetchDataService.json()
+
+  const FetchDataMenu= await fetch('http://localhost:3000/menu')
+  const resMenu = await FetchDataMenu.json()
+
+
+    return{
+      props:{resService,resMenu}
+    }
 }
