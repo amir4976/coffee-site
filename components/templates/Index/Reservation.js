08ca934320a6e11d69e924c06e8cc071a3c18dfc
amir4976 <amirhosianahmadi4976@gmail.com>
2023-11-10 22:12:37 +0330

    reservation component

--- components/templates/Index/Reservation.js
+++ components/templates/Index/Reservation.js
@@ -1,6 +1,29 @@
-import React from 'react'
-
+import React,{useRef, useState} from 'react'
 function Reservation() {
+  const [UseName,setUseName]= useState(null)  
+  const [UseEmail,setUseEmail] = useState(null)
+  const [UseDate,setUseDate] = useState(null)
+  const [UseTime,setUseTime] = useState(null)
+  const [UsePerson,setUsePerson] = useState(null)
+   const addReservationToDb = async (e)=>{
+    e.preventDefault()
+    console.log(UsePerson)
+    const FetchData = await fetch('http://localhost:3000/reservation',{
+      method:"POST",
+      headers:{
+          "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        name:UseName,
+        email:UseEmail,
+        date:UseDate,
+        time:UseTime,
+        persons:UsePerson
+      })
+    })
+    console.log(FetchData)
+
+   }
   return (
     <div className="container-fluid my-5">
         <div className="container">
@@ -26,25 +49,25 @@ function Reservation() {
                             <h1 className="text-white mb-4 mt-5">Book Your Table</h1>
                             <form className="mb-5">
                                 <div className="form-group">
-                                    <input type="text" className="form-control bg-transparent border-primary p-4" placeholder="Name"
+                                    <input onChange={(e)=>setUseName(e.target.value)} type="text" className="form-control bg-transparent border-primary p-4" placeholder="Name"
                                         required="required" />
                                 </div>
                                 <div className="form-group">
-                                    <input type="email" className="form-control bg-transparent border-primary p-4" placeholder="Email"
+                                    <input onChange={(e)=>setUseEmail(e.target.value)} type="email" className="form-control bg-transparent border-primary p-4" placeholder="Email"
                                         required="required" />
                                 </div>
                                 <div className="form-group">
                                     <div className="date" id="date" data-target-input="nearest">
-                                        <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Date" data-target="#date" data-toggle="datetimepicker"/>
+                                        <input onChange={(e)=>setUseDate(e.target.value)} type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Date" data-target="#date" data-toggle="datetimepicker"/>
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <div className="time" id="time" data-target-input="nearest">
-                                        <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker"/>
+                                        <input onChange={(e)=>setUseTime(e.target.value)} type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker"/>
                                     </div>
                                 </div>
                                 <div className="form-group">
-                                    <select className="custom-select bg-transparent border-primary px-4" style={{height: '49px'}}>
+                                    <select onChange={(e)=>setUsePerson(e.target.value)} className="custom-select bg-transparent border-primary px-4" style={{height: '49px'}}>
                                         <option selected>Person</option>
                                         <option value="1">Person 1</option>
                                         <option value="2">Person 2</option>
@@ -54,7 +77,7 @@ function Reservation() {
                                 </div>
                                 
                                 <div>
-                                    <button className="btn btn-primary btn-block font-weight-bold py-3" type="submit">Book Now</button>
+                                    <button onClick={addReservationToDb} className="btn btn-primary btn-block font-weight-bold py-3" type="submit">Book Now</button>
                                 </div>
                             </form>
                         </div>
