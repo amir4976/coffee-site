a1424316c3408da79572662e85a28a2f6b03cdfe
amir4976 <amirhosianahmadi4976@gmail.com>
2023-11-12 19:34:00 +0330

    testimonial section

--- components/templates/Index/Testimonial.js
+++ components/templates/Index/Testimonial.js
@@ -1,9 +1,23 @@
+import TestimonialItem from '@/components/modules/TestimonialItem/TestimonialItem'
 import React from 'react'
 
-function Testimonial() {
+function Testimonial({comments}) {
+  console.log(comments)
   return (
-    <div>
-      
+<div class="container-fluid py-5">
+        <div class="container">
+            <div class="section-title">
+                <h4 class="text-primary text-uppercase" style={{letterSpacing: '5px'}}>Testimonial</h4>
+                <h1 class="display-4">Our Clients Say</h1>
+            </div>
+            <div class="owl-carousel testimonial-carousel">
+              {comments.map((comment)=>(
+                <>
+                    <TestimonialItem comment={comment} />
+                </>
+              ))}
+            </div>
+        </div>
     </div>
   )
 }
