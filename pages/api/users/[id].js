1f1a0584e54e08e092bef814fc66124b2f359b73
amir h ahamdi <93821178+amir4976@users.noreply.github.com>
2023-11-24 16:50:13 +0330

    api

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,21 +1,42 @@
+import path from "path";
+import fs from "fs";
+import users from "@/data/db";
 
-import users from "@/data/db"
-
-
-const handler =(req,res)=>{
-    
-
-    if (req.method === "GET"){
-        const {id}= req.query;
-        console.log(id)
-        const user = users.find(user=>user.id == +id)
-        if(user){
-            res.json(user)            
-        }else{
-            res.status(404)
-            return res.json({"message":'user not found'})
-        }
+const handler = (req, res) => {
+  if (req.method === "GET") {
+    const { id } = req.query;
+    console.log(id);
+    const user = users.find((user) => user.id == +id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404);
+      return res.json({ message: "user not found" });
     }
-}
+  } else if (req.method === "DELETE") {
+    const { id } = req.query;
+
+    // and we find a path for our database using path Method
+    const dbPath = path.join(process.cwd(), "data", "db.json");
+
+    // and then we have to use the dbPath to find out db.json file to see the data
+    const dbFilesystem = fs.readFileSync(dbPath);
+
+    // because the dbFileSystem give us an unreadable code we use json.parse to parse it and make it readable
+    const parsedData = JSON.parse(dbFilesystem);
+
+    // so we find our user inside the fucking database and then we fucking deleted you know wite filter method 
+    const findUser = parsedData.users.filter((user) => user.id !== id);
+    
+    // like always we set this to err variable and check it baby😎
+    const err =  fs.writeFileSync(dbPath,JSON.stringify({...parsedData,users:findUser}))
+    if(err){
+        res.json(err)
+    }else{
+        res.json('user deleted successfully !!!')
+    }
+    
+  }
+};
 
 export default handler;
